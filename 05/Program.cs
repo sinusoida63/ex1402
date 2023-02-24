008ff15b0a0ba09c4716f2257701fd2db77c866a
Dinarae <shunusovadinara@rambler.ru>
2023-02-24 11:11:40 +0400

    05

---
+++ 05/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("ВВедите имя пользователя: ");
+string username = Console.Readline();
+
+if (username.ToLower() == "маша");
+{
+    Console.WriteLine("Ура , это же Маша");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
