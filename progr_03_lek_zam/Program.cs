ee7680e4e14c01e3b60fe8551574b39357a10b5b
Dinarae <shunusovadinara@rambler.ru>
2023-03-11 09:16:19 +0400

    progr 03 замена символа

---
+++ progr_03_lek_zam/Program.cs
@@ -0,0 +1,20 @@
+﻿string text = "- Я думаю, - сказал князь";
+
+
+string Replace(string text,char oldValue, char newValue)
+{
+    string result = string.Empty;
+
+int length= text.Length;
+for (int i =0; i<length; i++)
+{
+    if(text[i] == oldValue) result= result  +$"{newValue}";
+    else result = result  + $"{text[i]}";
+}
+return result;
+}
+string newText = Replace(text, ' ', '|');
+Console.WriteLine(newText);
+Console.WriteLine();
+newText = Replace(newText, 'к', 'К');
+Console.WriteLine(newText);
