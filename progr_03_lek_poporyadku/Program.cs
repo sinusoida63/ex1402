48d77c958a14144d671cc0a5686e96d246ee06ee
Dinarae <shunusovadinara@rambler.ru>
2023-03-11 10:06:41 +0400

    03 по порядку

---
+++ progr_03_lek_poporyadku/Program.cs
@@ -0,0 +1,17 @@
+﻿void selectionSort(int[] array)
+{
+for (int i = 0; i < array.Length - 1; i++)
+{
+int minPosition = i;
+for (int j = i+1; j < array.Length; j++)
+{
+if(array[j] < array[minPosition]) minPosition = j;
+{
+minPosition = j;
+}
+}
+int temporary = array[i];
+array[i] = array[minPosition];
+array[minPosition] = temporary;
+}
+}
