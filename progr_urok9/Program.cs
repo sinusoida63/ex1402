8629ee4728f17ed6c5a282cad18a9a975ffa04f3
Dinarae <shunusovadinara@rambler.ru>
2023-03-18 15:12:49 +0400

    урок 9. ИТОГОВОЕ Задача 64: Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1. Выполнить с помощью рекурсии.

---
+++ progr_urok9/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача 64: Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1. 
+// Выполнить с помощью рекурсии.
+
+int GetValue(string text)
+{
+  Console.Write(text);
+  return Convert.ToInt32(Console.ReadLine());
+}
+
+string numbersFromNToOne(int number)
+{
+  if (number > 0) return $"|{number}|" + numbersFromNToOne(number - 1);
+  else return String.Empty;
+}
+
+int n = GetValue("Введите значение N: ");
+Console.Write(numbersFromNToOne(n));
