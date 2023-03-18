4a1ab4d2a0b33e0d23a12511ca83187a1bb37d1b
Dinarae <shunusovadinara@rambler.ru>
2023-03-18 15:25:24 +0400

    урок 9. ИТОГОВОЕ Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N. Выполнить с помощью рекурсии.

---
+++ progr_urok9_zadacha66/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+//Выполнить с помощью рекурсии.
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+int GetValue(string text)
+{
+  Console.Write(text);
+  return Convert.ToInt32(Console.ReadLine());
+}
+
+int SumRec(int number1, int number2)
+{
+  if (number2 == number1 - 1) return 0;
+  else return number2 + SumRec(number1, number2 - 1);
+}
+
+int m = GetValue("Введите значение M: ");
+int n = GetValue("Введите значение N: ");
+
+Console.WriteLine($"\nСумма натуральных чисел в промежутке от {m} до {n} = {SumRec(m, n)}");
