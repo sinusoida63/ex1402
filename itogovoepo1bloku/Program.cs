c1d1e488302314a40f59085120a8a005ccd8ad2a
Dinarae <shunusovadinara@rambler.ru>
2023-03-19 08:49:37 +0400

    ИТОГОВОЕ. Выбор специализации. IT-специалист

---
+++ itogovoepo1bloku/Program.cs
@@ -0,0 +1,25 @@
+﻿// Написать программу, которая из имеющегося массива строк формирует новый массив из строк, длина которых меньше, либо равна 3 символам. 
+// Первоначальный массив можно ввести с клавиатуры, либо задать на старте выполнения алгоритма. 
+// При решении не рекомендуется пользоваться коллекциями, лучше обойтись исключительно массивами.
+// Примеры:
+// [“Hello”, “2”, “world”, “:-)”] → [“2”, “:-)”]
+// [“1234”, “1567”, “-2”, “computer science”] → [“-2”]
+// [“Russia”, “Denmark”, “Kazan”] → []
+
+string[] array = { "Hello", "2", "world", ":-" };
+// string[] array = {"1234", "1567", "-2", "computer science"};
+// string[] array = {"Russia", "Denmark", "Kazan"};
+
+string[] result = new string[array.Length];
+int count = 0;
+
+for (int i = 0; i < array.Length; i++)
+{
+    if (array[i].Length <= 3)
+    {
+        result[count] = array[i];
+        count++;
+    }
+}
+Console.WriteLine(string.Join(" ",result));
+Console.ReadKey();
